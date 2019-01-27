c0e53be7f918953f5250d550a0ac63da9f5ed871
Delphine Millet <delphine.millet@zenika.com>
2019-01-27 20:53:20 +0100

    :sparkles: images

--- src/pages/images/images.jsx
+++ src/pages/images/images.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 
+import logo from './img/logo.png'
+import decorative from './img/decorative.jpg'
+import meaningful from './img/meaningful.jpg'
+import './styles.css'
+
 const Images = () => (
   <div>
     <h1 className="title">Images</h1>
+    <div className="column">
+      {/* alternative textuelle au logo : nom de la marque */}
+      <div>Powered by <img src={logo} className="logo" /></div>
+
+      <div>
+        {/* laisser vide l'attribut alt pour les images purement décoratives */}
+        <img src={decorative} className="image" />
+
+        {/* décrire le contexte / le contenu / le sentiment véhiculé par l'image
+            si elle apporte de l'information
+        */}
+        <img src={meaningful} className="image" />
+      </div>
+    </div>
   </div>
 )
- 
+
 export default Images
